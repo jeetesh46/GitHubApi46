677d3966ad103efa1affb9d463c0128e5c63c852
JEETESH PAVAN <jeeteshpavan@jeeteshs-air.mynetworksettings.com>
2023-03-21 21:12:31 -0400

    changes made for github api

--- test.py
+++ test.py
@@ -1,58 +1,61 @@
 import unittest
 from unittest.mock import MagicMock, patch
-from nose.tools import assert_true, assert_false
+from nose.tools import assert_dict_equal, assert_true, assert_false
 
 from github import github_fetch_data
 
-class Github(unittest.TestCase):
+class TestGithub(unittest.TestCase):
 
     @patch('github.requests.get')
     def test_github_fetch_data_with_mocked_response(self, mock_get):
-        # creating a mock response
+        # create a mock response
         mock_response = MagicMock()
         mock_response.text = '[{"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]'
+        mock_response.status_code = 200
         mock_get.return_value = mock_response
-        mock_get.return_value.status_code = 200
-        # calling the function
+
+        # call the function
         response = github_fetch_data('testing1')
 
-        # asserting the output
+        # assert the output
         expected_output = {'repo1': 3, 'repo2': 3, 'repo3': 3}
-        self.assertEqual(response, expected_output)
+        assert_dict_equal(response, expected_output)
 
     @patch('github.requests.get')
     def test_github_fetch_data_getting_called(self, mock_get):
-        # creating a mock response
-        mock_get.return_value = MagicMock()
-        mock_get.return_value.text = '[{"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]'
-        mock_get.return_value.status_code = 200
-        # calling the function
-        response = github_fetch_data('test1')
+        # create a mock response
+        mock_response = MagicMock()
+        mock_response.text = '[{"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]'
+        mock_response.status_code = 200
+        mock_get.return_value = mock_response
 
+        # call the function
+        github_fetch_data('test1')
+
+        # assert that requests.get was called
         assert_true(mock_get.called)
 
     @patch('github.requests.get')
     def test_github_fetch_data_not_getting_called(self, mock_get):
-        # creating a mock response
-        mock_get.return_value = MagicMock()
-        mock_get.return_value.text = '[{"name": "repo1"}, {"name": "repo2"}, {"name": "repo3"}]'
-        mock_get.return_value.status_code = 200
-        # calling the function
-        # response = github_fetch_data('jeetesh46')
+        # call the function with a real user id
+        github_fetch_data('jeetesh46')
 
+        # assert that requests.get was not called
         assert_false(mock_get.called)
 
     @patch('github.requests.get')
     def test_github_fetch_data_with_response_400(self, mock_get):
-        mock_get.return_value = MagicMock()
-        mock_get.return_value.text = '{"error": "status is 400"}'
-        mock_get.return_value.status_code = 400
-        # calling the function with a real user id
+        # create a mock response with status code 400
+        mock_response = MagicMock()
+        mock_response.text = '{"error": "status is 400"}'
+        mock_response.status_code = 400
+        mock_get.return_value = mock_response
+
+        # call the function with a real user id
         response = github_fetch_data('abc')
 
-        # asserting that the response is not empty
-        self.assertEqual({}, {})
-
+        # assert that the response is empty
+        assert_dict_equal(response, {})
 
 if __name__ == '_main_':
     unittest.main()
